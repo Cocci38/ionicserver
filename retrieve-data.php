06739f9430eb1a4c1bd341117f0cd04bf7195c4f
Sandra <sandra.villot038@gmail.com>
2022-09-12 09:00:20 +0200

    amélioration sécurisation

--- retrieve-data.php
+++ retrieve-data.php
@@ -12,54 +12,38 @@ $password = "";
 
 // Créer une instance de la classe PDO (connexion à la base)
 try {
-$conn = new PDO("mysql:host=$host;dbname=$db_name; charset=UTF8", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// echo "Connexion réussi";
+    $conn = new PDO("mysql:host=$host;dbname=$db_name; charset=UTF8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connexion réussi";
 } catch (PDOException $exception) {
     echo "Erreur de connexion : " . $exception->getMessage();
 }
-// $id = intval($_GET["id_object"]);
 // Prépare et exécute la requête de lecture de la table (try/catch)
-// switch ($status) {
-//     case 'get':
-    if (isset($_GET['id'])) {
-        try {
-            $select = $conn->prepare("SELECT * FROM foundlost WHERE id_object = " . $_GET['id']);
-            // $select->bindValue(":id", $_GET['id']);
-            $select->execute();
-            $result =$select->fetch(PDO::FETCH_ASSOC);
-            echo json_encode($result);
-            }
-            catch(PDOException$e) { 
-                echo $e->getMessage(); 
-            }
-        
-    } else {
-        try {
-            $select = $conn->prepare("SELECT * FROM foundlost ORDER BY date DESC");
-            $select->execute();
-            // 
-            while($row = $select->fetch(PDO::FETCH_OBJ) )
-            { // Assign each row of data to associat
-                    $data[] = $row;
-            }
-            // $select->execute();
-            // $result =$select->fetchAll(PDO::FETCH_OBJ);
-            // $myJSON = json_encode($data);
-            // echo "<pre>",print_r($_GET['status'] ,1),"</pre>";die();
-                echo json_encode($data);
-            }
-            catch(PDOException$e) { echo $e->getMessage(); 
-            }
-    }
-    
 
-        // echo $result;
-//         break;
-//     case '1':
-//         $select = $conn->prepare("SELECT * FROM foundlost WHERE id_object = :id");
-//         $select->execute();
-//         $result =$select->fetchAll(PDO::FETCH_ASSOC);
-//         break;
-// }
-?>
+if (isset($_GET['id'])) {
+    // error_log($_GET['id']);
+    $id = htmlspecialchars(strip_tags(trim(stripslashes($_GET['id']))));
+    try {
+        $select = $conn->prepare("SELECT * FROM foundlost WHERE id_object = $id");
+        $select->bindValue("id_object", $id, PDO::PARAM_INT);
+        $select->execute();
+        $result = $select->fetch(PDO::FETCH_ASSOC);
+        echo json_encode($result);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+} else {
+    try {
+        $select = $conn->prepare("SELECT * FROM foundlost ORDER BY date DESC");
+        $select->execute();
+        // 
+        while ($row = $select->fetch(PDO::FETCH_OBJ)) { // Assign each row of data to associat
+            $data[] = $row;
+        }
+        // echo "<pre>",print_r($_GET['status'] ,1),"</pre>";die();
+        echo json_encode($data);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
