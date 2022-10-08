7d404fd4aa535261873df0b838316d4910ccfddb
Sandra <sandra.villot038@gmail.com>
2022-10-08 14:48:06 +0200

    ajout commentaire

--- retrieve-data.php
+++ retrieve-data.php
@@ -7,15 +7,6 @@ header('Access-Control-Allow-Origin: *');
 
 require_once 'configuration.php';
 
-// Créer une instance de la classe PDO (connexion à la base)
-// try {
-//     $conn = new PDO("mysql:host=". DB_HOST . ";dbname=".DB_NAME."; charset=UTF8", DB_USER, DB_PWD);
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     // echo "Connexion réussi";
-// } catch (PDOException $exception) {
-//     echo "Erreur de connexion : " . $exception->getMessage();
-// }
-
 // Si $_GET['id'] est défini on sélectionne par l'id
 if (isset($_GET['id'])) {
     // error_log(print_r($_GET, 1));
