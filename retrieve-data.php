f311ecf1f83c81f7198504bfd9c6a4d28fd63942
Sandra <sandra.villot038@gmail.com>
2022-10-26 08:44:07 +0200

    affichage image fonctionnel

--- retrieve-data.php
+++ retrieve-data.php
@@ -12,7 +12,10 @@ if (isset($_GET['id'])) {
     // error_log(print_r($_GET, 1));
     $id = htmlspecialchars(strip_tags(trim(stripslashes($_GET['id']))));
     try {
-        $select = $conn->prepare("SELECT id_object, status, description, date, location, firstname, lastname, email, user_id FROM objects WHERE id_object = $id");
+        $select = $conn->prepare("SELECT id_object, status, description, date, location, firstname, lastname, email, user_id, picture
+        FROM objects 
+        INNER JOIN pictures ON objects.id_object = object_id
+        WHERE id_object = $id");
         $select->bindParam("id_object", $id, PDO::PARAM_INT);
         $select->execute();
         $result = $select->fetch(PDO::FETCH_ASSOC);
@@ -23,7 +26,10 @@ if (isset($_GET['id'])) {
 // Sinon je sélectionne tous et je trie par la date en ordre descendant
 } else {
     try {
-        $select = $conn->prepare("SELECT id_object, status, description, date, location, firstname, lastname, email, user_id FROM objects ORDER BY date DESC");
+        $select = $conn->prepare("SELECT id_object, status, description, date, location, firstname, lastname, email, user_id, picture 
+        FROM objects 
+        INNER JOIN pictures ON objects.id_object = object_id 
+        ORDER BY date DESC");
         $select->execute();
         while ($row = $select->fetch(PDO::FETCH_ASSOC)) { 
             $data[] = $row;
